98d9e85bb7abc5f32f8639debfd7afb894eae94a
César Henrique de Figueiredo <cesar.sprite@gmail.com>
2023-01-22 19:09:27 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { prisma } from '@prisma/client'
+import { appRoutes } from './routes'
+const app = Fastify()
+
+
+
+app.register(cors)
+app.register(appRoutes)
+
+
+app.listen({
+    port: 3333,
+    host: "0.0.0.0"
+}).then(() => {
+    console.log('Http server running!')
+})
